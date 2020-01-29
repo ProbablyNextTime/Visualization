76bf8cbb28507c693042adb056c6954fb994cb04
Shya <rumarenko3@gmail.com>
2020-01-29 14:58:48 -0800

    animejs check

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import  anime from "animejs";
+
+function Test (){
+
+    useEffect( () => {
+        let a1 =  anime({
+          targets: 'div.box.yellow',
+            zIndex: -1,
+          translateX: [
+              {value: 300, duration: 1000},
+              {value: 0, duration: 1000}
+          ],
+            easing : "linear"
+        });
+        let a2 =  anime({
+          targets: 'div.box.green',
+          translateX: [
+              {value: -300, duration: 1000},
+              {value: 0, duration: 1000}
+          ],
+            easing : "linear"
+        });
+        a1.play();
+        a2.play();
+    })
+
+    return (
+        <div className="wrapper">
+            <div className="box yellow">1</div>
+            <div className="box green">2</div>
+        </div>
+    )
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+        <Test/>
     </div>
   );
 }
